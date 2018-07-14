048eef09d0ed3677ea6a0de98b94536ca128503b
bfchengnuo <bfchengnuo@gmail.com>
2018-07-14 16:24:51 +0800

    + 对接支付宝当面付（未测试）

---
+++ src/main/java/com/bfchengnuo/ssmall/controller/portal/OrderController.java
@@ -0,0 +1,96 @@
+package com.bfchengnuo.ssmall.controller.portal;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
+import com.bfchengnuo.ssmall.common.Const;
+import com.bfchengnuo.ssmall.common.ResponseCode;
+import com.bfchengnuo.ssmall.common.ServerResponse;
+import com.bfchengnuo.ssmall.pojo.User;
+import com.bfchengnuo.ssmall.service.IOrderService;
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * 订单管理接口，包括订单的支付
+ * Created by 冰封承諾Andy on 2018/7/14.
+ */
+@Controller
+@RequestMapping("order")
+public class OrderController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+    @Autowired
+    private IOrderService orderService;
+
+    @GetMapping("pay.do")
+    @ResponseBody
+    public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        String path = request.getServletContext().getRealPath("upload");
+        return orderService.pay(user.getId(), orderNo, path);
+    }
+
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+        Map<String, String[]> requestMap = request.getParameterMap();
+        // 使用迭代器的话推荐使用 for
+        // for (Iterator<String> iterator = requestMap.keySet().iterator(); iterator.hasNext(); ){}
+        requestMap.forEach((k, v) -> {
+            StringBuilder valStr = new StringBuilder();
+            for (int length = v.length - 1; length >= 0; length--) {
+                valStr.append(length == 0 ? v[length] : v[length] + ",");
+            }
+            params.put(k, valStr.toString());
+        });
+        LOGGER.info("支付宝回调，sign: {}, trade_status: {}, 参数: {}", params.get("sign"), params.get("trade_status"), params.toString());
+        // 验证支付宝回调的正确性，以及避免重复回调
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(),"utf-8",Configs.getSignType());
+
+            if(!alipayRSACheckedV2){
+                return ServerResponse.createByErrorMessage("非法请求,验证不通过");
+            }
+        } catch (AlipayApiException e) {
+            LOGGER.error("支付宝验证回调异常",e);
+        }
+
+        //todo 验证各种数据
+
+        ServerResponse serverResponse = orderService.aliCallback(params);
+        if(serverResponse.isSuccess()){
+            return Const.AlipayCallback.RESPONSE_SUCCESS;
+        }
+        return Const.AlipayCallback.RESPONSE_FAILED;
+    }
+
+    @RequestMapping("query_order_pay_status.do")
+    @ResponseBody
+    public ServerResponse<Boolean> queryOrderPayStatus(HttpSession session, Long orderNo){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+
+        ServerResponse serverResponse = orderService.queryOrderPayStatus(user.getId(),orderNo);
+        if(serverResponse.isSuccess()){
+            return ServerResponse.createBySuccess(true);
+        }
+        return ServerResponse.createBySuccess(false);
+    }
+}
---
+++ src/main/java/com/bfchengnuo/ssmall/service/IOrderService.java
@@ -0,0 +1,17 @@
+package com.bfchengnuo.ssmall.service;
+
+import com.bfchengnuo.ssmall.common.ServerResponse;
+
+import java.util.Map;
+
+/**
+ * Created by 冰封承諾Andy on 2018/7/14.
+ */
+public interface IOrderService {
+
+    ServerResponse pay(Integer userId, Long orderNo, String path);
+
+    ServerResponse aliCallback(Map<String, String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId, Long orderNo);
+}
